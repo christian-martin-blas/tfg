b59a7d931a18a9bc2c33fd2a0ac53a0227cbaca2
christian-martin-blas <christian.martin.blas@gmail.com>
2014-10-16 19:44:46 +0200

    Arreglado codigo para que coja el nombre de la Base en adminTool

--- admin/uploader.php
+++ admin/uploader.php
@@ -6,17 +6,18 @@
 <?php
 	include '../ChromePhp.php';
 
-	//ChromePhp::log("hola");
+	//ChromePhp::log($_POST['nombreBase']);
 
 	$target_path = "../img/admin";
 	if($_FILES['fileBase']['name'] != "") {
 	//Caso en que se sube una base y unas particiiones desde 0
-		$base_name = substr($_FILES['fileBase']['name'], 0, -4);
+		if($_POST['nombreBase'] != "") $base_name = $_POST['nombreBase'];
 		uploadImage($target_path . "/bases", "fileBase");
-		//ChromePhp::log($_FILES['fileMiniatura']['name']);
-		if($_FILES['fileMiniatura']['name'] != "") uploadImage("../img/admin/miniaturas/", "fileMiniatura");
+		$path_particiones = "../img/admin/miniaturas/" . $base_name;
+		ChromePHP::log(mkdir($path_particiones, 0700));
+		if($_FILES['fileMiniatura']['name'] != "") uploadImage("../img/admin/miniaturas/" . $base_name . "/", "fileMiniatura");
 		$path_particiones = "../img/admin/particiones/" . $base_name;
-		mkdir($path_particiones, 0700);
+		ChromePHP::log(mkdir($path_particiones, 0700));
 		if($_FILES['fileParticion1']['name'] != "") uploadImage($path_particiones, "fileParticion1");
 		if($_FILES['fileParticion2']['name'] != "") uploadImage($path_particiones, "fileParticion2");
 		if($_FILES['fileParticion3']['name'] != "") uploadImage($path_particiones, "fileParticion3");
@@ -30,7 +31,6 @@
 
 	function uploadImage($target_path, $file) {
 		$tmp_path = $target_path . "/" . basename($_FILES[$file]['name']);
-		ChromePhp::log($tmp_path);
 		if(move_uploaded_file($_FILES[$file]['tmp_name'], $tmp_path)) {
 		    echo "El fichero ".  basename( $_FILES[$file]['name']). 
 		    " se ha subido.";
