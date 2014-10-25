b8f80b43bf1b97b4443d8cbf03c38db10a5353cf
christian-martin-blas <christian.martin.blas@gmail.com>
2014-10-25 20:07:40 +0200

    Creado fichero php con los alerts de vuelta de los submits

--- index.php
+++ index.php
@@ -445,66 +445,7 @@
           <button id="buttonPopUpDownload" onclick="downloadImage()">Descargar Escudo</button>
         </div>
         <?php
-          if(isset($_GET['success']))
-          {
-            $success = $_GET['success'];
-            echo '<script language="javascript">';
-            if($success == 1) echo '$("<div title=\'Información\'><b>Se ha guardado correctamente el escudo.</b></div>").dialog({
-                                                                          modal: true,
-                                                                          buttons: {
-                                                                            Ok: function() {
-                                                                              $( this ).dialog( "close" );
-                                                                            }
-                                                                          }
-                                                                        });';
-            if($success == 2) echo '$("<div title=\'Información\'><b>Se ha borrado correctamente el escudo.</b></div>").dialog({
-                                                                          modal: true,
-                                                                          buttons: {
-                                                                            Ok: function() {
-                                                                              $( this ).dialog( "close" );
-                                                                            }
-                                                                          }
-                                                                        });';
-            echo '</script>';
-          }
-          else if(isset($_GET['error'])) {
-            $error = $_GET['error'];
-            //Se ha subido una particion sin subir otra antes.
-            echo '<script language="javascript">';
-            if($error == 1) echo '$("<div title=\'Atención\'><b>No se ha podido conectar con la Base de datos.</b></div>").dialog({
-                                                                          modal: true,
-                                                                          buttons: {
-                                                                            Ok: function() {
-                                                                              $( this ).dialog( "close" );
-                                                                            }
-                                                                          }
-                                                                        });';
-            if($error == 2) echo '$("<div title=\'Atención\'><b>No se ha podido guardar en la Base de datos.</b></div>").dialog({
-                                                                          modal: true,
-                                                                          buttons: {
-                                                                            Ok: function() {
-                                                                              $( this ).dialog( "close" );
-                                                                            }
-                                                                          }
-                                                                        });';
-            if($error == 3) echo '$("<div title=\'Atención\'><b>Ya existe un escudo con ese nombre.</b></div>").dialog({
-                                                                          modal: true,
-                                                                          buttons: {
-                                                                            Ok: function() {
-                                                                              $( this ).dialog( "close" );
-                                                                            }
-                                                                          }
-                                                                        });';
-            if($error == 4) echo '$("<div title=\'Atención\'><b>No se ha podido borrar el escudo de la Base de datos.</b></div>").dialog({
-                                                                          modal: true,
-                                                                          buttons: {
-                                                                            Ok: function() {
-                                                                              $( this ).dialog( "close" );
-                                                                            }
-                                                                          }
-                                                                        });';
-            echo '</script>';
-          }
+          include './jqueryAlerts.php';
         ?>
         <div id="workspace">
           <canvas id="mainCanvas" width="530" height="350">
